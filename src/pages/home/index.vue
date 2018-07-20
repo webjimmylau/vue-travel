8f8e5d10fce45af4e47a627f7f2cfa250a0aa65c
webjimmylau <web_jimmy_lau@126.com>
2018-07-20 17:59:12 +0800

    add icons

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
 
 export default {
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons,
   }
 }
 </script>

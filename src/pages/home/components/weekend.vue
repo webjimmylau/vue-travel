d384fcc106fd9730f22324616a606f678964a6c1
webjimmylau <web_jimmy_lau@126.com>
2018-07-24 10:11:21 +0800

    add weekend.vue

---
+++ src/pages/home/components/weekend.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="recommend" v-if="list && list.length">
+    <div class="recommend-title" v-show="title">{{title}}</div>
+    <div class="recommend-cont">
+      <div class="recommend-item" v-for="item in recommendList" :key="item.id">
+        <div class="recommend-img">
+          <img :src="item.imgUrl">
+        </div>
+        <div class="recommend-info">
+          <div class="info-box">
+            <h1 class="info-name">{{item.name}}</h1>
+            <div class="info-item">
+              <span class="info-address">{{item.address}}</span>
+              <span class="info-comment"><span>{{item.comment}}</span>条评论</span>
+            </div>
+            <div class="info-item info-bottom">
+              <span class="info-price"><i class="price-symbol"></i><span>{{item.price}}</span></span>
+              <router-link class="info-link" to="">查看详情</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Weekend",
+    props: ['title', 'list'],
+    data(){
+      return {
+        recommendList: this.list
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .recommend
+    margin-top: .16rem
+    background: #fff
+    .recommend-title
+      padding-top .16rem
+      font-size: .36rem
+      line-height: 2
+      text-indent: .16rem
+    .recommend-cont
+      .recommend-item
+        display flex
+        padding .16rem
+        border-bottom: $borderDefault
+        .recommend-img
+          width: 2rem
+          img
+            width: 100%
+        .recommend-info
+          flex: 1
+          padding-left .16rem
+          .info-box
+            position: relative
+            height: 2rem
+            .info-bottom
+              position: absolute
+              bottom: 0
+              width: 100%
+          .info-item
+            display: -webkit-flex;
+            justify-content: space-between;
+            margin-top: .16rem
+          .info-name
+            font-size: .3rem
+            line-height: 1.5
+            max-height: .9rem
+            overflow: hidden
+          .info-comment span
+            margin-right: .04rem
+            color: $orange
+          .info-price
+            align-self: flex-end
+            span
+              font-size: .36rem
+              color: $orangeRed
+          .price-symbol:after
+            content: '¥'
+            color: $orangeRed
+          .info-link
+            display inline-block
+            padding: .12rem
+            color: #fff
+            background $orange
+            border-radius: $borderRadius
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,6 +4,7 @@
     <home-banner></home-banner>
     <home-icons></home-icons>
     <home-recommend :list="recommendList" title="热门推荐"></home-recommend>
+    <home-weekend :list="recommendList" title="周末去哪儿"></home-weekend>
   </div>
 </template>
 
@@ -12,6 +13,7 @@
   import HomeBanner from './components/banner'
   import HomeIcons from './components/icons'
   import HomeRecommend from './components/recommend'
+  import HomeWeekend from './components/weekend'
 
   export default {
     name: 'Home',
@@ -20,6 +22,7 @@
       HomeBanner,
       HomeIcons,
       HomeRecommend,
+      HomeWeekend,
     },
     data(){
       return {
@@ -29,6 +32,32 @@
             imgUrl: 'https://imgs.qunarzz.com/sight/p0/1607/6c/6ce376cd5775ce6fb4.img.jpg_200x200_7ed7ce79.jpg',
             name: '九道谷漂流',
             comment: '23',
+            price: 120,
+            address: '九道谷',
+          },
+          {
+            id: '00002',
+            imgUrl: 'https://imgs.qunarzz.com/sight/p0/1707/fd/fd38ff549c0ee891a3.img.jpg_200x200_52e7f8b4.jpg',
+            name: '佛山游得酷水上乐园',
+            comment: '186',
+            price: 160,
+            address: '顺德区',
+          },
+          {
+            id: '00003',
+            imgUrl: 'https://imgs.qunarzz.com/sight/p0/1708/67/67b574e957302bb2a3.water.jpg_200x200_c4d09d8d.jpg',
+            name: '西樵平沙岛水上乐园',
+            comment: '37',
+            price: 100,
+            address: '南海平沙',
+          }
+        ],
+        weekendList: [
+          {
+            id: '00001',
+            imgUrl: 'http://img1.qunarzz.com/sight/source/1505/92/580e9ea4f37a1b.jpg_r_640x214_72112761.jpg',
+            name: '广州必游TOP10',
+            comment: '感受现代与传统相融合的广州',
             price: 120,
             address: '九道谷',
           },

9bfe0d8718147ef851673d01d8b82a10f18abae0
webjimmylau <web_jimmy_lau@126.com>
2018-07-31 18:30:23 +0800

    add detail banner gallery

---
+++ src/common/gallery/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="gallery" v-if="show" @click="handleChangeClick">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in galleryData" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      show: Boolean
+    },
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          paginationType: 'fraction',
+          autoplay: 5000,
+        },
+        galleryData: [
+          {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1807/77/77570cc3ebf7d9d8a3.img.jpg_r_800x800_95763221.jpg'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1807/29/29bd2b91ce0b1e0ea3.water.jpg_r_800x800_a742c8be.jpg'
+          },
+          {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1807/4b/4bc18f98357e98eda3.water.jpg_r_800x800_04e943d4.jpg'
+          }
+        ]
+      }
+    },
+    methods: {
+      handleChangeClick(){
+        this.$emit('change', !this.show);
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .gallery >>>
+  .swiper-container
+    position: fixed
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    color: #fff
+    background: #000
+    .swiper-slide
+      display: flex
+      align-items: center
+      .swiper-img
+        width: 100%
+    .swiper-pagination
+      bottom: 1rem
+
+</style>
--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,52 +1,75 @@
 <template>
   <div class="banner">
-    <div class="banner-img">
-      <img src="http://img1.qunarzz.com/sight/p0/1807/77/77570cc3ebf7d9d8a3.img.jpg_600x330_ca3cdd3a.jpg">
-    </div>
-    <div class="banner-info">
-      <div class="banner-title">长鹿休闲度假农庄(AAAAA景区)</div>
-      <div class="banner-num">
-        <span class="iconfont icon-img">&#xe716;</span>
-        <span>82</span>
+    <div class="banner-cont" @click="handleClick">
+      <div class="banner-img">
+        <img src="http://img1.qunarzz.com/sight/p0/1807/77/77570cc3ebf7d9d8a3.img.jpg_600x330_ca3cdd3a.jpg">
+      </div>
+      <div class="banner-info">
+        <div class="banner-title">长鹿休闲度假农庄(AAAAA景区)</div>
+        <div class="banner-num">
+          <span class="iconfont icon-img">&#xe716;</span>
+          <span>82</span>
+        </div>
       </div>
     </div>
+    <common-gallery :show="isShowGallery" @change="handleChange"></common-gallery>
   </div>
 </template>
 
 <script>
-  export default {}
+  import CommonGallery from 'common/gallery'
+  export default {
+    data(){
+      return {
+        isShowGallery: false
+      }
+    },
+    methods: {
+      handleClick(){
+        this.isShowGallery = true;
+      },
+      handleChange(val){
+        console.log(val)
+        this.isShowGallery = val;
+      }
+    },
+    components: {
+      CommonGallery
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
   @import "~styles/varibles.styl"
   .banner
-    position relative
-    .banner-img
-      height: 0
-      padding-bottom: 55%
-      overflow: hidden
-      img
-        width: 100%
-    .banner-info
-      display: flex
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .6rem
-      line-height: .6rem
-      color: #fff
-      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
-      .banner-title
-        flex: 1
-        padding-left .12rem
-      .banner-num
-        height: .4rem
-        line-height: .4rem
-        margin: .1rem .12rem 0 0
-        padding: 0 .2rem
-        background: $bgColor
-        border-radius: .2rem
-        .icon-img
-          vertical-align: top
+    .banner-cont
+      position relative
+      .banner-img
+        height: 0
+        padding-bottom: 55%
+        overflow: hidden
+        img
+          width: 100%
+      .banner-info
+        display: flex
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .6rem
+        line-height: .6rem
+        color: #fff
+        background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+        .banner-title
+          flex: 1
+          padding-left .12rem
+        .banner-num
+          height: .4rem
+          line-height: .4rem
+          margin: .1rem .12rem 0 0
+          padding: 0 .2rem
+          background: $bgColor
+          border-radius: .2rem
+          .icon-img
+            vertical-align: top
 </style>

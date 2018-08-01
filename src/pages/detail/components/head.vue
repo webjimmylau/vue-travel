b55f115355f9fd06792f50e5e5b2046f493191a0
webjimmylau <web_jimmy_lau@126.com>
2018-08-01 15:28:17 +0800

    给全局滚动事件解绑 deactivated组件被移除时调用

--- src/pages/detail/components/head.vue
+++ src/pages/detail/components/head.vue
@@ -43,6 +43,9 @@
     },
     activated(){
       window.addEventListener('scroll', this.handleScroll);
+    },
+    deactivated(){
+      window.removeEventListener('scroll', this.handleScroll);
     }
   }
 </script>

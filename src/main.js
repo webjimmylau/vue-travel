730217dd5696c9b7e258111d0ece5f784959b4ba
webjimmylau <web_jimmy_lau@126.com>
2018-08-03 11:40:30 +0800

    真机测试和移动设备bug修复

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import BScroll from 'better-scroll'
 
 import fastClick from 'fastclick' // 为消除移动端浏览器，从物理触摸到触发点击事件之间的300ms延时的问题
 import VueAwesomeSwiper from 'vue-awesome-swiper' // 轮播插件
+import 'babel-polyfill' // 解决低版本浏览器出现的一些白屏问题，ie9和一些低版本的高级浏览器对es6新语法并不支持
 
 import 'styles/reset.css' // 将所有html标签的默认样式统一化
 import 'styles/border.css' // 为解决不同的像素密度，1px在各种设备下表现不同的问题
--- src/pages/city/components/bar.vue
+++ src/pages/city/components/bar.vue
@@ -6,7 +6,7 @@
       :key="item"
       :ref="item"
       @click="handleClickLetter"
-      @touchstart="handleTouchStart"
+      @touchstart.prevent="handleTouchStart"
       @touchmove="handleTouchMove"
       @touchend="handleTouchEnd">{{item}}
     </div>
